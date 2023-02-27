f6fe77bd0323e85c9ba251767c16d858fe32849c
MasakiDesktop <mandij50@gmail.com>
2023-02-26 22:02:57 -0500

    reaction model

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,32 @@
 const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new Schema.Types.ObjectId //is this right?
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            //280 max characters
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdTime) => {
+                return createdTime
+            }
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
